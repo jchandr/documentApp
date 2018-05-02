bb01bd980abc58caeeca1a24a89ea12aae8b4ecf
Jeyabalaji Chandrasekaran <jeyabalajic93@gmail.com>
2018-05-02 14:56:56 -0700

    added bootstrap, added navbar
    Signed-off-by: Jeyabalaji Chandrasekaran <jeyabalajic93@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {AlertModule} from 'ngx-bootstrap/alert';
 
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
